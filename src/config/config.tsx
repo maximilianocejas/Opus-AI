746e297301de04b6135c038507df29e7b088bfca
Maximiliano Cejas <maxicejas12354@gmail.com>
2025-09-29 22:46:44 -0300

    update gemini version

--- src/config/config.tsx
+++ src/config/config.tsx
@@ -5,7 +5,7 @@ const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_API_KEY as string)
 
 export default async function GeneratePromptAI (textPrompt : string){
     try{
-        const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash"});
+        const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash"});
         const result = await model.generateContent(textPrompt);
         const text = result.response.text();;
         return{
